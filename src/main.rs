65dfe11388c71e8aef08140c037f653e93abf3c8
kaRaGODDD <triplesss1@mail.ru>
2026-06-11 01:03:40 +0000

    подключил новые штуки

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader}
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1_cracker: <wordlist.txt> <sha1_hash>");
+        return;
+    }
 }
